165fe0f8346b24017f16332afa300f05039c1027
Jack King <ledoanquoc12c3@gmail.com>
2022-04-14 09:02:55 +0700

    complete category shop

--- CaseStudy/Template/molla-eCommerce-html-template 2/category.js
+++ CaseStudy/Template/molla-eCommerce-html-template 2/category.js
@@ -1,35 +1,42 @@
-function getAllCategory() {
-    $.ajax({
-        type: 'GET',
-        url: `http://localhost:8080/categories`,
-        success: function (categories) {
-            let content='';
-            for (let i = 0; i < categories.length; i++) {
-                content += `<div class="filter-item">
-                                <div class="custom-control">
-                                       <a href=""><label class="custom-control">${categories[i].name}</label></a>
-                                </div><!-- End .custom-checkbox -->
-                                    <span class="item-count" id="${categories[i].name}"></span>
-                            </div>`
-
-            };
-
-            $('#category_list').html(content);
-
-        }
-    })
-}
-
-function showProductAmount(){
-    $.ajax({
-        type:'GET',
-        url: `http://localhost:8080/products`,
-        success: function(){
-
-        }
-    })
-}
-$(document).ready(function () {
-    getAllCategory();
-
-})
+//
+// function getAllCategory() {
+//     $.ajax({
+//         type: 'GET',
+//         url: `http://localhost:8080/categories`,
+//         success: function (categories) {
+//             let content='';
+//             for (let i = 0; i < categories.length; i++) {
+//                 content += `<div class="filter-item">
+//                                 <div class="custom-control">
+//                                        <a href="" onclick=""><label class="custom-control">${categories[i].name}</label></a>
+//                                 </div><!-- End .custom-checkbox -->
+//                                     <span class="item-count" id="${categories[i].name}"></span>
+//                             </div>`
+//
+//             };
+//
+//             $('#category_list').html(content);
+//
+//         }
+//     })
+// }
+// function showProductList(id){
+//     $.ajax({
+//         type:'GET',
+//         url:`http://localhost:8080/categories/${id}`,
+//         success: function(products){
+//
+//         }
+//     })
+// }
+// function showProductAmount(){
+//     $.ajax({
+//         type:'GET',
+//         url: `http://localhost:8080/products`,
+//         success: function(){
+//         }
+//     })
+// }
+// $(document).ready(function () {
+//     getAllCategory();
+// })
--- CaseStudy/Template/molla-eCommerce-html-template 2/product.js
+++ CaseStudy/Template/molla-eCommerce-html-template 2/product.js
@@ -1,19 +1,16 @@
-
-function getAllProduct(page) {
-    $.ajax({
-        type: 'GET',
-        url: `http://localhost:8080/products?page=${page}`,
-        success: function (data) {
-            let content='';
-            let pagination='';
-            let products=data.content;
-            for (let i = 0; i < products.length; i++) {
-                content += `<div class="col-6 col-md-4 col-lg-4 col-xl-3" >
+let currentUser = localStorage.getItem('currentUser');
+currentUser = JSON.parse(currentUser);
+let searchParams = null;
+searchParams = new URLSearchParams(window.location.search)
+function showProductCard(data) {
+    let contentHtml='';
+    for (let i = 0; i < data.length; i++) {
+        contentHtml += `<div class="col-6 col-md-4 col-lg-4 col-xl-3" >
                                 <div class="product">
                                     <figure class="product-media">
                                         <span class="product-label label-new">New</span>
                                         <a href="product.html">
-                                            <img src="http://localhost:8080/image/${products[i].image}" alt="Product image" class="product-image">
+                                            <img src="http://localhost:8080/image/${data[i].image}" alt="Product image" class="product-image">
                                         </a>
 
                                         <div class="product-action-vertical">
@@ -31,9 +28,9 @@ function getAllProduct(page) {
                                         <div class="product-cat">
                                             <a href="#">Women</a>
                                         </div><!-- End .product-cat -->
-                                        <h3 class="product-title"><a href="product.html">${products[i].name}</a></h3><!-- End .product-title -->
+                                        <h3 class="product-title"><a href="product.html">${data[i].name}</a></h3><!-- End .product-title -->
                                         <div class="product-price">
-                                            ${products[i].price}
+                                            ${data[i].price} VND
                                         </div><!-- End .product-price -->
                                         <div class="ratings-container">
                                             <div class="ratings">
@@ -48,19 +45,192 @@ function getAllProduct(page) {
                                         </div><!-- End .product-nav -->
                                     </div><!-- End .product-body -->
                                 </div><!-- End .product -->
-                            </div>`;
+                            </div>`
 
 
-            };
+    }
+    ;
+    return contentHtml;
+}
 
-            $('#showProduct').html(content);
+function getAllProduct() {
+    $.ajax({
+        url: `http://localhost:8080/products`,
+        type: 'GET',
+        headers: {
+            'Authorization': 'Bearer ' + currentUser.token
+        },
+        success: function (data) {
+            let contentHtml='';
+            let products = data.content;
+            contentHtml = showProductCard(products);
+
+            $('#showProduct').html(contentHtml);
 
         }
     })
 }
 
 
-$(document).ready(function () {
-    getAllProduct(0);
+function getAllCategory() {
+    $.ajax({
+        type: 'GET',
+        url: `http://localhost:8080/categories`,
+        headers: {
+            'Authorization': 'Bearer ' + currentUser.token
+        },
+        success: function (categories) {
+            let content='';
+            for (let i = 0; i < categories.length; i++) {
+                content += `<div class="filter-item">
+                                <div class="custom-control">
+                                       <a href="" onclick=""><label class="custom-control">${categories[i].name}</label></a>
+                                </div><!-- End .custom-checkbox -->
+                                    <span class="item-count" id="${categories[i].name}"></span>
+                            </div>`
 
+            };
+
+            $('#category_list').html(content);
+
+        }
+    })
+}
+function showProductList(id){
+    $.ajax({
+        type:'GET',
+        url:`http://localhost:8080/categories/${id}`,
+        headers: {
+            'Authorization': 'Bearer ' + currentUser.token
+        },
+        success: function(products){
+
+        }
+    })
+}
+function showProductAmount(){
+    $.ajax({
+        type:'GET',
+        url: `http://localhost:8080/products`,
+        headers: {
+            'Authorization': 'Bearer ' + currentUser.token
+        },
+        success: function(){
+        }
+    })
+}
+
+function showCategoryIndex(){
+    $.ajax({
+        type:'GET',
+        url: `http://localhost:8080/categories`,
+        headers: {
+            'Authorization': 'Bearer ' + currentUser.token
+        },
+        success: function(categories){
+            let content='';
+            for (let i = 0; i <categories.length ; i++) {
+                content+=`<li><a href="category-boxed.html?categoryId=${categories[i].id}">${categories[i].name}</a></li>`
+            };
+            $('#category_index').html(content);
+        }
+    })
+}
+
+function showProductByCategoryId(id){
+
+    $.ajax({
+        type:'GET',
+        url: `http://localhost:8080/categories/view/${id}`,
+        headers: {
+            'Authorization': 'Bearer ' + currentUser.token
+        },
+        success: function(data){
+            let content='';
+            let categories = data.content;
+            content+=showProductCard(categories);
+            $('#showProduct').html(content);
+        }
+    })
+}
+
+function showShopList(){
+    $.ajax({
+        type:'GET',
+        url: `http://localhost:8080/shops`,
+        headers: {
+            'Authorization': 'Bearer ' + currentUser.token
+        },
+        success: function(shops){
+            let content='';
+            for (let i = 0; i < shops.length; i++) {
+                content+=`<li><a href="category-boxed.html?shopId=${shops[i].id}&shopName=${shops[i].name}">${shops[i].name}</a></li>`
+            }
+
+            $('#shop_list_index').html(content);
+        }
+    })
+}
+function showCategoryByShopId(shopId,shopName){
+    $.ajax({
+        type:'GET',
+        url: `http://localhost:8080/shops/view/${shopId}`,
+        headers: {
+            'Authorization': 'Bearer ' + currentUser.token
+        },
+        success: function(data){
+
+            let content='';
+            let categories = data.content;
+            for (let i = 0; i < categories.length; i++) {
+                content += `<div class="filter-item">
+                                <div class="custom-control">
+                                       <a><label class="custom-control">${categories[i].name}</label></a>
+                                </div><!-- End .custom-checkbox -->
+                                    <span class="item-count" id="${categories[i].name}"></span>
+                            </div>`
+
+            };
+            $('#shop_name').html(shopName);
+            $('#category_list').html(content);
+    }
+})
+}
+
+function getUrlParameter(sParam) {
+    let sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+    return false;
+};
+
+
+
+$(document).ready(function () {
+
+    showShopList();
+
+    if(searchParams.has('categoryId')==true){
+        let categoryId = searchParams.get('categoryId');
+        showProductByCategoryId(categoryId);
+        showCategoryIndex();
+    } else if(getUrlParameter('shopId')!==true){
+        let shopId= getUrlParameter('shopId');
+        let shopName=getUrlParameter('shopName');
+        showCategoryByShopId(shopId,shopName);
+        showProductByCategoryId(1);
+    }else{
+        getAllCategory();
+        getAllProduct();
+        showCategoryIndex();
+    }
 })
